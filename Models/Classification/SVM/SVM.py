892c7a5afb8c2c8fe4e0c280b68a2ee3933162e8
nakaya-shunsuke <1422528@ed.tus.ac.jp>
2022-10-04 11:47:35 +0900

    [Add]SoftMarginSVM

--- Models/Classification/SVM/SVM.py
+++ Models/Classification/SVM/SVM.py
@@ -14,7 +14,7 @@ class HardMarginSVM():
         self.num_features = x.shape[1]
         self.weight = np.zeros(self.num_features)
         self.b = 0
-        r_generator = np.random.RandomState(self.random_state) 
+        r_generator = np.random.RandomState(self.random_state) # random generator
         self.alpha = r_generator.normal(loc=0., scale=1e-2, size=self.num_samples) # From Normal Distribution
         
         for _ in range(self.epoch):
@@ -47,3 +47,61 @@ class HardMarginSVM():
         grad = np.ones(self.num_samples) - H @ self.alpha
         self.alpha += self.lr * grad
         self.alpha = np.where(self.alpha < 0, 0, self.alpha)
+        
+        
+class SoftMarginSVM():
+    def __init__(self, lr=0.001, epoch=10000, C=1., random_state=42):
+        self.lr = lr
+        self.epoch = epoch
+        self.C = C
+        self.random_state = random_state
+        self.is_trained = False
+        
+    
+    def fit(self, x, y):
+        self.num_samples = x.shape[0]
+        self.num_features = x.shape[1]
+        self.weight = np.zeros(self.num_features)
+        self.b = 0
+        r_generator = np.random.RandomState(self.random_state) # random generator
+        self.alpha = r_generator.normal(loc=0., scale=1e-2, size=self.num_samples) # From Normal Distribution
+        
+        for _ in range(self.epoch):
+            self._cycle(x, y)
+            
+        index_sv = []
+        index_inner = []
+        
+        for i in range(self.num_samples):
+            if 0 < self.alpha[i] < self.C:
+                index_sv.append(i)
+            elif self.alpha[i] == self.C:
+                index_inner.append(i)
+        
+        for i in index_sv + index_inner:
+            self.weight += self.alpha[i] * y[i] * x[i]
+            
+        for i in index_sv:
+            self.b += y[i] - (self.weight @ x[i]) # @ : inner product
+        self.b = self.b / len(index_sv)
+        
+        self.is_trained = True
+            
+        
+    def predict(self, X):
+        if not self.is_trained:
+            raise Exception('This model is not trained.')
+
+        hyperplane = X @ self.weight + self.b
+        result = np.where(hyperplane > 0, 1, -1)
+        return result
+        
+        
+    def _cycle(self, x, y):
+        y = y.reshape([-1, 1])
+        H = (y @ y.T) * (x @ x.T)
+        grad = np.ones(self.num_samples) - H @ self.alpha
+        self.alpha += self.lr * grad
+        # 0 < self.alpha < self.C (=lambda)
+        self.alpha = np.where(self.alpha < 0, 0, self.alpha)
+        self.alpha = np.where(self.alpha > self.C, self.C, self.alpha)
--- README.md
+++ README.md
@@ -10,15 +10,11 @@ ML_Scratch
 |   |   `-- SVM
 |   |       |-- SVM.py
 |   |       |-- Scratch_SVM.ipynb
-|   |       `-- __pycache__
-|   |           `-- SVM.cpython-38.pyc
 |   |-- Clustering
 |   `-- Regression
 |       `-- Linear_Regression
 |           |-- Linear_Regression.py
 |           |-- Scratch_Linear_Regression.ipynb
-|           `-- __pycache__
-|               `-- Linear_Regression.cpython-38.pyc
 |-- Preprocessing
 |   |-- Standard_Scaler.py
 |   `-- Train_Test_Split.py
