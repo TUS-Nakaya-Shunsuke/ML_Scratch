67ef241aa43dff06a0445a9d7a429363995a06ef
nakaya-shunsuke <1422528@ed.tus.ac.jp>
2022-11-03 10:13:10 +0900

    [Amend]RandomForest

--- Models/Classification/DecisionTree/DecisionTree.py
+++ Models/Classification/DecisionTree/DecisionTree.py
@@ -1,104 +1,223 @@
+from collections import Counter
 import numpy as np
+import pandas as pd
 
-def calculate_gini(data, label, feature_idx, threshold):
-    gini = 0
-    data_size = len(label)
+class Node:
 
-    # Division target left or right
-    # division_target = [label[data[:, feature_idx] >= threshold], label[data[:, feature_idx] < threshold]]
-    label_left = label[data[:, feature_idx] >= threshold]
-    label_right = label[data[:, feature_idx] < threshold]
+    def __init__(self):
+        self.left = None      # 左側に分岐した子ノード
+        self.right = None     # 右側に分岐した子ノード
+        self.feature = None   # 分岐条件の特徴量
+        self.value = None     # 分岐条件のしきい値
+        self.label = None     # 自身の予測ラベル
+        self.gain = None      # 自身の情報ゲイン
 
-    score_left = 0
-    score_right = 0
-    classes = np.unique(label)
+    def build(self, X, y):  # X : pd.Dataframe, y : pd.Series
 
-    for c in classes:
-        if len(label_left) != 0:
-            score_left += float((np.sum(label_left == c) / len(label_left))**2)
-        if len(label_right) != 0:
-            score_right += float((np.sum(label_right == c) / len(label_right))**2)
+        features = X.columns
+        n_features = len(features)
 
-    gini = ((1 - score_left)*len(label_left) + (1 - score_right)*len(label_right)) / data_size
+        if len(y.unique()) == 1:
+            self.label = y.unique()[0]
+            return
 
-    return gini
+        class_counts = Counter(y)  # ラベルの各クラスの数をdict型で返す
+        self.label = max(class_counts, key=class_counts.get)
 
+        # 最適な分岐条件を保持するための変数
+        best_gain = 0.0
+        best_feature = None
+        best_value = None
 
-def search_best_split(data, label):
-    features = data.shape[1]
-    best_threshold = None
-    best_feature_idx = None
-    gini = None
-    gini_min = 1
+        gini = self.gini(y)  # 情報ゲインの計算に必要
 
-    for feature_idx in range(features):
-        values = data[:, feature_idx]
-        for value in values:
-            gini = calculate_gini(data, label, feature_idx, value)
-            if gini_min > gini:
-                gini_min = gini
-                best_threshold = value
-                best_feature_idx = feature_idx
+        for f in features:
+            values = X[f].unique()
 
-    return gini_min, best_threshold, best_feature_idx
+            for val in values:
+                y_l = y[X[f] >= val]
+                y_r = y[X[f] <  val]
 
+                gain = self.information_gain(y_l, y_r, gini)
+                if gain > best_gain:
+                    best_gain = gain
+                    best_feature = f
+                    best_value = val
 
-class DecisionTreeNode():
-    def __init__(self, data, label, max_depth):
-        self.data = data
-        self.label = label
-        self.max_depth = max_depth
-        self.left = None
-        self.right = None
-        self.depth = None
-        self.threshold = None
-        self.feature_idx = None
-        self.gini_min = None
-        if len(self.label) != 0:
-            self.output = np.argmax(np.bincount(self.label))
+        # 情報ゲインが増えていなければ終了
+        if best_gain == 0:
+            return
         else:
-            self.output = None
+            self.gain = best_gain
+            self.feature = best_feature
+            self.value = best_value
+
+            # 左側のノードを再帰的に構築
+            X_l = X[X[self.feature] >= self.value]
+            y_l = y[X[self.feature] >= self.value]
+            self.left = Node()
+            self.left.build(X_l, y_l)
+
+            # 右側のノードを再帰的に構築
+            X_r = X[X[self.feature] < self.value]
+            y_r = y[X[self.feature] < self.value]
+            self.right = Node()
+            self.right.build(X_r, y_r)
+
+    # ジニ不純度の計算
+    def gini(self, y):
+        classes = y.unique()
+        gini = 1.0
+        for c in classes:
+            gini -= (len(y[y==c]) / len(y)) ** 2
+        return gini
+
+    # 情報ゲインの計算
+    def information_gain(self, y_l, y_r, current_uncertainty):
+        pl = float(len(y_l) / (len(y_l)+len(y_r)))
+        pr = float(len(y_r) / (len(y_l)+len(y_r)))
+        return current_uncertainty - (pl*self.gini(y_l) + pr*self.gini(y_r))
+
+    # 一つのデータに対する予測
+    def predict(self, datum):
+        # 葉ノードでなければ分岐条件に従って分岐する
+        if self.feature != None:
+            if datum[self.feature] >= self.value:
+                return self.left.predict(datum)
+            else:
+                return self.right.predict(datum)
+        # 葉ノードなら自身の予測ラベルを返す
+        else:
+            return self.label
+        
+class DecisionTree:
+
+    def __init__(self):
+        self.root = None
+
+    def fit(self, X, y):
+        self.root = Node()
+        self.root.build(X, y)
+
+    def predict(self, X):
+        y_pred = []
+        for i in range(len(X)):
+            y_pred.append(self.root.predict(X.iloc[i]))
+        return pd.Series(y_pred)
+
+
+
+# import numpy as np
+
+# def calculate_gini(data, label, feature_idx, threshold):
+    
+#     gini = 0
+#     data_size = len(label)
+
+#     # Division target left or right
+#     # Division_target = [label[data[:, feature_idx] >= threshold], label[data[:, feature_idx] < threshold]]
+#     label_left = label[data[:, feature_idx] >= threshold]
+#     label_right = label[data[:, feature_idx] < threshold]
+
+#     score_left = 0
+#     score_right = 0
+#     classes = np.unique(label)
+
+#     for c in classes:
+#         if len(label_left) != 0:
+#             score_left += float((np.sum(label_left == c) / len(label_left))**2)
+#         if len(label_right) != 0:
+#             score_right += float((np.sum(label_right == c) / len(label_right))**2)
+
+#     gini = ((1 - score_left)*len(label_left) + (1 - score_right)*len(label_right)) / data_size
+
+#     return gini
+
+
+# def search_best_split(data, label):
+    
+#     features = data.shape[1]
+#     best_threshold = None
+#     best_feature_idx = None
+#     gini = None
+#     gini_min = 1
+
+#     for feature_idx in range(features):
+#         values = data[:, feature_idx]
+#         for value in values:
+#             gini = calculate_gini(data, label, feature_idx, value)
+#             if gini_min > gini:
+#                 gini_min = gini
+#                 best_threshold = value
+#                 best_feature_idx = feature_idx
+
+#     return gini_min, best_threshold, best_feature_idx
+
+
+# class DecisionTreeNode():
+    
+#     def __init__(self, data, label, max_depth):
+#         self.data = data
+#         self.label = label
+#         self.max_depth = max_depth
+#         self.left = None
+#         self.right = None
+#         self.depth = None
+#         self.threshold = None
+#         self.feature_idx = None
+#         self.gini_min = None
+#         self.output = np.argmax(np.bincount(self.label))
         
     
-    def fit(self, depth):
-        self.depth = depth
-        self.gini_min, self.threshold, self.feature_idx = search_best_split(self.data, self.label)
+#     def fit(self, depth):
         
-        if self.depth == self.max_depth or self.gini_min == 0 or len(self.label) == 0:
-            return
+#         if len(np.unique(self.label)) == 1:
+#             self.label = np.unique(self.label)
+#             return
         
-        left_idx = self.data[:, self.feature_idx] >= self.threshold
-        right_idx = self.data[:, self.feature_idx] < self.threshold
+#         self.depth = depth
+#         self.gini_min, self.threshold, self.feature_idx = search_best_split(self.data, self.label)
         
-        self.left = DecisionTreeNode(self.data[left_idx], self.label[left_idx], self.max_depth)
-        self.right = DecisionTreeNode(self.data[right_idx], self.label[right_idx], self.max_depth)
-        self.left.fit(self.depth + 1)
-        self.right.fit(self.depth + 1)
+#         if self.depth == self.max_depth or self.gini_min == 0 or len(self.label) == 0:
+#             return
+        
+#         left_idx = self.data[:, self.feature_idx] >= self.threshold
+#         right_idx = self.data[:, self.feature_idx] < self.threshold
+        
+#         self.left = DecisionTreeNode(self.data[left_idx], self.label[left_idx], self.max_depth)
+#         self.right = DecisionTreeNode(self.data[right_idx], self.label[right_idx], self.max_depth)
+#         self.left.fit(self.depth + 1)
+#         self.right.fit(self.depth + 1)
         
         
-    def predict(self, data):
-        if self.gini_min == 0. or self.depth == self.max_depth or len(self.label) == 0:
-            return self.output
+#     def predict(self, data):
         
-        else:
-            if data[self.feature_idx] > self.threshold:
-                return self.left.predict(data)
-            else:
-                return self.right.predict(data)
+#         if len(np.unique(self.label)):
+#             return self.label
+        
+#         if self.gini_min == 0. or self.depth == self.max_depth or len(self.label) == 0:
+#             return self.output
+        
+#         else:
+#             if data[self.feature_idx] > self.threshold:
+#                 return self.left.predict(data)
+#             else:
+#                 return self.right.predict(data)
             
             
-class DecisionTreeClassifier():
-    def __init__(self, max_depth=3):
-        self.max_depth = max_depth
-        self.tree = None
+# class DecisionTreeClassifier():
+    
+#     def __init__(self, max_depth=3):
+#         self.max_depth = max_depth
+#         self.tree = None
    
-    def fit(self, data, label):
-        initial_depth = 0
-        self.tree = DecisionTreeNode(data, label, self.max_depth)
-        self.tree.fit(initial_depth)
+#     def fit(self, data, label):
+#         initial_depth = 0
+#         self.tree = DecisionTreeNode(data, label, self.max_depth)
+#         self.tree.fit(initial_depth)
    
-    def predict(self, data):
-        pred = []
-        for d in data:
-            pred.append(self.tree.predict(d))
-        return np.array(pred)
+#     def predict(self, data):
+#         pred = []
+#         for d in data:
+#             pred.append(self.tree.predict(d))
+#         return np.array(pred)
--- Models/Classification/DecisionTree/RandomForest.py
+++ Models/Classification/DecisionTree/RandomForest.py
@@ -1,59 +1,119 @@
 import numpy as np
-from DecisionTree import DecisionTreeClassifier
+import pandas as pd
+from DecisionTree import DecisionTree
 
 class RandomForest:
-    
-    def __init__(self, n_trees=10, max_depth=3):
+
+    def __init__(self, n_trees=10):
         self._n_trees = n_trees
-        self.max_depth = max_depth
         self._forest = [None] * self._n_trees
         self._used_features_set = [None] * self._n_trees
-        
-    
-    def fit(self, x, y, random_state=0):
+
+    def fit(self, X, y, random_state=0):  # X : pd.DataFrame, y : pd.Series
         self._classes = np.unique(y)
-        sampled_x, sampled_y = self._bootstrap_sample(x, y, random_state=random_state)
-        for i, (x_i, y_i) in enumerate(zip(sampled_x, sampled_y)):
-            each_tree = DecisionTreeClassifier(max_depth=self.max_depth)
-            each_tree.fit(x_i, y_i)
-            self._forest[i] = each_tree
-        
-        
-    def _bootstrap_sample(self, x, y, random_state=0):
-        n_features = x.shape[1]
+        sampled_X, sampled_y = self._bootstrap_sample(X, y, random_state=random_state)
+        for i, (sampled_Xi, sampled_yi) in enumerate(zip(sampled_X, sampled_y)):
+            tree = DecisionTree()
+            tree.fit(sampled_Xi, sampled_yi)
+            self._forest[i] = tree
+
+    # データセットから `._n_trees` 個のサブセットを返す関数
+    def _bootstrap_sample(self, X, y, random_state=0):
+
+        n_features = len(X.columns)
         n_features_forest = int(np.floor(np.sqrt(n_features)))
-        sampled_x = []
-        sampled_y = []
+        bootstrapped_X = []
+        bootstrapped_y = []
+
         np.random.seed(random_state)
-        
-        for i in range(self._n_trees):  # for each tree
+        for i in range(self._n_trees):
             idx = np.random.choice(len(y), size=len(y))
-            feature_idx = np.random.choice(n_features, size=n_features_forest, replace=False)
-            sampled_x.append(x[idx][:, feature_idx])
-            sampled_y.append(y[idx])
-            self._used_features_set[i] = feature_idx
-        
-        return sampled_x, sampled_y
-    
-    
-    def predict(self, x):
-        proba = self.predict_proba(x)
+            col_idx = np.random.choice(n_features, size=n_features_forest, replace=False)
+            bootstrapped_X.append(X.iloc[idx, col_idx])
+            bootstrapped_y.append(y.iloc[idx])
+            self._used_features_set[i] = col_idx
+        return bootstrapped_X, bootstrapped_y
+
+    def predict(self, X):
+        proba = self.predict_proba(X)
         return self._classes[np.argmax(proba, axis=1)]
-    
-    
-    def predict_proba(self, x):
+
+    def predict_proba(self, X):
         if self._forest[0] is None:
-            raise ValueError("Model has not fitted yed.")
-            
-        votes = []
+            raise ValueError('Model not fitted yet')
+
+        # 決定木群による投票
+        votes = pd.DataFrame()
         for i, (tree, used_features) in enumerate(zip(self._forest, self._used_features_set)):
-            votes.append(tree.predict(x[:, used_features]))
-        votes_array = np.array(votes)
-        
-        votes_result_array = np.zeros((len(x), len(self._classes)), dtype=int)
+            votes[i] = tree.predict(X.iloc[:, used_features])
+        votes_array = votes.values
+
+        # 投票結果を集計
+        counts_array = np.zeros((len(X), len(self._classes)), dtype=int)
         for c in self._classes:
-            votes_result_array[:, c] = np.sum(np.where(votes_array==c, 1, 0), axis=0)  # sum result for trees
-            
-        proba = votes_result_array / self._n_trees
-        
+            counts_array[:, c] = np.sum(np.where(votes_array==c, 1, 0), axis=1)
+
+        # 予測クラス毎に割合を計算し、probaとして返す
+        proba = counts_array / self._n_trees
         return proba
+
+    
+# import numpy as np
+# from DecisionTree import DecisionTreeClassifier
+
+# class RandomForest:
+    
+#     def __init__(self, n_trees=10, max_depth=3):
+#         self._n_trees = n_trees
+#         self.max_depth = max_depth
+#         self._forest = [None] * self._n_trees
+#         self._used_features_set = [None] * self._n_trees
+        
+    
+#     def fit(self, x, y, random_state=0):
+#         self._classes = np.unique(y)
+#         sampled_x, sampled_y = self._bootstrap_sample(x, y, random_state=random_state)
+#         for i, (x_i, y_i) in enumerate(zip(sampled_x, sampled_y)):
+#             each_tree = DecisionTreeClassifier(max_depth=self.max_depth)
+#             each_tree.fit(x_i, y_i)
+#             self._forest[i] = each_tree
+        
+        
+#     def _bootstrap_sample(self, x, y, random_state=0):
+#         n_features = x.shape[1]
+#         n_features_forest = int(np.floor(np.sqrt(n_features)))
+#         sampled_x = []
+#         sampled_y = []
+#         np.random.seed(random_state)
+        
+#         for i in range(self._n_trees):  # for each tree
+#             idx = np.random.choice(len(y), size=len(y))
+#             feature_idx = np.random.choice(n_features, size=n_features_forest, replace=False)
+#             sampled_x.append(x[idx][:, feature_idx])
+#             sampled_y.append(y[idx])
+#             self._used_features_set[i] = feature_idx
+        
+#         return sampled_x, sampled_y
+    
+    
+#     def predict(self, x):
+#         proba = self.predict_proba(x)
+#         return self._classes[np.argmax(proba, axis=1)]
+    
+    
+#     def predict_proba(self, x):
+#         if self._forest[0] is None:
+#             raise ValueError("Model has not fitted yed.")
+            
+#         votes = []
+#         for i, (tree, used_features) in enumerate(zip(self._forest, self._used_features_set)):
+#             votes.append(tree.predict(x[:, used_features]))
+#         votes_array = np.array(votes)
+        
+#         votes_result_array = np.zeros((len(x), len(self._classes)), dtype=int)
+#         for c in self._classes:
+#             votes_result_array[:, c] = np.sum(np.where(votes_array==c, 1, 0), axis=0)  # sum result for trees
+            
+#         proba = votes_result_array / self._n_trees
+        
+#         return proba
